2a048aa6e7d4d0e09b2d11588784ad3de4885105
Harjot <harjot@harkul.com>
2022-04-17 16:46:01 +0100

    feat: rename display name of AD application

--- identity/index.ts
+++ identity/index.ts
@@ -3,9 +3,9 @@ import { Application, ServicePrincipal } from '@pulumi/azuread'
 
 import * as environment from '../helpers/environment'
 
-export const application = new Application( `${environment.name}-app`, { displayName: `App (${environment.name})` } )
+export const application = new Application( `${environment.name}-environment-app`, { displayName: `${environment.name}-environment-app` } )
 
-export const servicePrincipal = new ServicePrincipal( `${environment.name}-service-principal`, {
+export const servicePrincipal = new ServicePrincipal( `${environment.name}-environment-service-principal`, {
   applicationId: application.applicationId,
 } )
 
