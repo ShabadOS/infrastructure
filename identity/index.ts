fa11e80a755f0dfdb1987746406cbac818e97900
Harjot Singh <harjot@harkul.com>
2022-04-23 16:08:52 +0100

    fix(identity): export service principal password

--- identity/index.ts
+++ identity/index.ts
@@ -1,5 +1,5 @@
 import { ResourceGroup } from '@pulumi/azure-native/resources'
-import { Application, ServicePrincipal } from '@pulumi/azuread'
+import { Application, ServicePrincipal, ServicePrincipalPassword } from '@pulumi/azuread'
 
 import * as environment from '../helpers/environment'
 
@@ -7,6 +7,11 @@ export const application = new Application( `${environment.name}-environment-app
 
 export const servicePrincipal = new ServicePrincipal( `${environment.name}-environment-service-principal`, {
   applicationId: application.applicationId,
+} )
+
+export const servicePrincipalPassword = new ServicePrincipalPassword( `${environment.name}-environment-service-principal-password`, {
+  servicePrincipalId: servicePrincipal.id,
+  endDate: '2099-01-01T00:00:00Z',
 } )
 
 export const resourceGroup = new ResourceGroup( environment.name )
