e4e2244669867ce01a12bf16119e489fbe08f363
Harjot Singh <harjot@harkul.com>
2022-04-23 21:40:11 +0100

    refactor: move from top-level await to export = async
    Recommended by Pulumi docs as top-level awaits are not possible yet, since we require CommonJS module system

--- identity/index.ts
+++ identity/index.ts
@@ -3,15 +3,19 @@ import { Application, ServicePrincipal, ServicePrincipalPassword } from '@pulumi
 
 import * as environment from '../helpers/environment'
 
-export const application = new Application( `${environment.name}-environment-app`, { displayName: `${environment.name}-environment-app` } )
+export = async () => {
+  const application = new Application( `${environment.name}-environment-app`, { displayName: `${environment.name}-environment-app` } )
 
-export const servicePrincipal = new ServicePrincipal( `${environment.name}-environment-service-principal`, {
-  applicationId: application.applicationId,
-} )
+  const servicePrincipal = new ServicePrincipal( `${environment.name}-environment-service-principal`, {
+    applicationId: application.applicationId,
+  } )
 
-export const servicePrincipalPassword = new ServicePrincipalPassword( `${environment.name}-environment-service-principal-password`, {
-  servicePrincipalId: servicePrincipal.id,
-  endDate: '2099-01-01T00:00:00Z',
-} )
+  const servicePrincipalPassword = new ServicePrincipalPassword( `${environment.name}-environment-service-principal-password`, {
+    servicePrincipalId: servicePrincipal.id,
+    endDate: '2099-01-01T00:00:00Z',
+  } )
 
-export const resourceGroup = new ResourceGroup( environment.name )
+  const resourceGroup = new ResourceGroup( environment.name )
+
+  return { application, servicePrincipal, servicePrincipalPassword, resourceGroup }
+}
