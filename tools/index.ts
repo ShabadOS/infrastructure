1dceb5f95d84f08f7f05b2436e7ac6996aa070fc
Harjot Singh <harjot@harkul.com>
2023-10-23 00:46:24 +0100

    feat(tools): add iam module to assign reader role to service principal

--- tools/index.ts
+++ tools/index.ts
@@ -2,12 +2,14 @@ import azureModule from '../shared/azure'
 import identityModule from '../shared/identity'
 import azureSecretsModule from './azure-secrets'
 import codeSigningSecretsModule from './code-signing-secrets'
+import iamModule from './iam'
 import keyVaultModule from './key-vault'
 
 const stack = async () => {
   const azure = await azureModule()
 
   const identity = await identityModule()
+  await iamModule( { identity } )
 
   const keyVault = await keyVaultModule( { azure, identity } )
 
